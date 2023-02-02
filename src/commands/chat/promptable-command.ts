760ffa6e745d815e930da10ccfd87a3d2aed66ef
Justin Liang <justinliang1020@gmail.com>
2023-02-02 15:00:46 -0600

    added openAI call

--- src/commands/chat/promptable-command.ts
+++ src/commands/chat/promptable-command.ts
@@ -1,10 +1,14 @@
 import { ChatInputCommandInteraction, PermissionsString } from 'discord.js';
 import { RateLimiter } from 'discord.js-rate-limiter';
+import dotenv from 'dotenv';
+import { Configuration, OpenAIApi } from 'openai';
 
 import { Language } from '../../models/enum-helpers/index.js';
 import { Lang } from '../../services/index.js';
 import { InteractionUtils } from '../../utils/index.js';
 import { Command, CommandDeferType } from '../index.js';
+
+dotenv.config();
 
 export class PromptableCommand implements Command {
     public names = [Lang.getRef('chatCommands.promptable', Language.Default)];
@@ -16,10 +20,21 @@ export class PromptableCommand implements Command {
         let args = {
             prompt: intr.options.getString('prompt'),
         }
-        console.log(args);
-        console.log(intr.user.username);
+
+        // call OpenAI API
+        const configuration = new Configuration({
+            apiKey: process.env.OPENAI_API_KEY,
+        })
+        const openai = new OpenAIApi(configuration);
+        const completion = await openai.createCompletion({
+            model: 'text-davinci-003',
+            prompt: args.prompt,
+            temperature: 0.3
+        });
+
         const response = `${intr.user.username}'s prompt: ${args.prompt}
-Response: This is a response for the prompt, ${args.prompt}`;
+Response: This is the OpenAI API completion for the prompt: ${completion.data.choices[0].text}`;
+        
         await InteractionUtils.send(intr, response);
     }
 }
